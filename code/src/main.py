7c0f4b8148643a63025718f5ec933b5d76696eb6
Keerthana Purushotham <40817362+keerthanap8898@users.noreply.github.com>
2025-10-21 06:26:55 -0700

    Update main.py

--- code/src/main.py
+++ code/src/main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from tkinter.filedialog import askopenfilename
 from fuzzywuzzy import fuzz 
@@ -12,29 +8,44 @@ from code import extract_details as ed
 from code import ruleset_generation as rg
 
 
-# In[2]:
 
 
 filename1,filename2,ds,dd=sts.select_files()
 
-
-# In[3]:
+## OUTPUT 1: (pop up window)
+"""
+-> Select the source file from the pop up window.
+-> Select the destination file from the pop up window.
+"""
+#--------------------------------------------------------------------------------------------------------------------------
 
 
 path,pack,source,dest,so,de=ed.extract_attr(filename1,filename2,ds,dd)
+
 dicts=ed.extract_source_attr(ds)
 dictd=ed.extract_dest_attr(dd)
 
-
-# In[4]:
+## OUTPUT 2:
+"""
+C:/Users/kp/workspace/com.extol.ebi.demo01.singlefftoff/src/com/extol/ebi/demo01/singlefftoff
+com.extol.ebi.demo01.singlefftoff
+DelimitedFF
+FixedFF
+Source
+Target
+{'FirstName': 'String', 'LastName': 'String', 'Quantity': 'Integer', 'Color': 'String', 'UnitPrice': 'Decimal'}
+{'FullName': 'String', 'TotalPrice': 'Decimal', 'ColorCode': 'String', 'Units': 'Integer'}
+"""
+#--------------------------------------------------------------------------------------------------------------------------
 
 
 finalmap=rg.predict_map(dicts,dictd)
 RSpath=rg.generate_RS(path,pack,source,dest,so,de,finalmap)
 
-
-# In[ ]:
-
-
-
-
+## OUTPUT 3: Final Result
+"""
+{'FullName': ['FirstName', 'LastName'], 'TotalPrice': ['UnitPrice'], 'ColorCode': ['Color'], 'Units': ['Quantity', 'UnitPrice']}
+The ruleset file has been generated in:
+C:\\Users\\kp\\workspace\\com.extol.ebi.demo01.singlefftoff\\src\\com\\extol\\ebi\\demo01\\singlefftoff\\DelimitedFFtoFixedFFRS.ruleset
+"""
+#--------------------------------------------------------------------------------------------------------------------------
